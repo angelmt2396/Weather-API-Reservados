3e4030471c229509cb655b2ac0f376a892c12a36
angelmt2396 <angelmorantuz@gmail.com>
2023-12-18 01:49:45 -0600

    api reservamos

---
+++ app/controllers/weather_api_controller.rb
@@ -0,0 +1,29 @@
+class WeatherApiController < ApplicationController
+  require 'json'
+  require 'concurrent'
+
+  def index
+    begin
+      filter = params[:city]
+    cities = ReservamosCitiesService.getCities(filter)
+    cityWeather = []
+    threads = cities.map do |city|
+      Thread.new do
+        lat = city[:lat]
+        lon = city[:lon]
+        puts "lat: #{lat}, lon: #{lon}"
+        weather = OpenWeatherApiService.get(lat, lon)
+        cityWeather.push({
+          id: city[:id],
+          city: city[:city_name],
+          weather_forecast: weather
+        })
+      end
+    end
+    threads.each(&:join)
+    render json: cityWeather
+    rescue StandardError => e
+      render json: { error: "An error occurred: #{e.message}" }, status: :internal_server_error
+    end
+  end
+end
