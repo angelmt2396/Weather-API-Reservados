048cb628a369d69a80c79a12851c86aafaf4afc1
angelmt2396 <angelmorantuz@gmail.com>
2023-12-18 01:49:03 -0600

    api openweathermap

---
+++ app/services/open_weather_api_service.rb
@@ -0,0 +1,35 @@
+class OpenWeatherApiService
+  require 'net/http'
+  require 'uri'
+  def self.get(lat, lon)
+    lat = Float(lat)
+    lon = Float(lon)
+    uri_open_weather_map = 'https://api.openweathermap.org/data/2.5/onecall'
+    api_key = 'a5a47c18197737e8eeca634cd6acb581'
+    uri = URI.parse("#{uri_open_weather_map}?lat=#{lat}&lon=#{lon}&appid=#{api_key}")
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true
+    request = Net::HTTP::Get.new(uri.path + '?' + uri.query)
+    response = http.request(request)
+    if response.is_a?(Net::HTTPSuccess)
+      result = JSON.parse(response.body)
+      days = result['daily']
+      data = []
+      days.each do |day|
+        temp = day['temp']
+        data.push({
+          dt: Time.at(day['dt']),
+          tem_max: temp['max'],
+          tem_min: temp['min'],
+        })
+      end
+      return data
+    else
+      puts "Error: #{response.code} - #{response.message}"
+      return {
+        message: response.message,
+        code: response.code
+      }
+    end
+  end
+end
